0c214a9bf63740c46c40d0b2fac58c1fb7c1f82b
yourahmeddev <ahmedhassansheikh7@gmail.com>
2024-07-13 03:34:23 +0500

    migration completed model will start

--- database/migrations/2024_07_12_212649_create_extras_table.php
+++ database/migrations/2024_07_12_212649_create_extras_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('extras', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->string('title');
-            $table->text('description');
-            $table->boolean('is_publish')->default(false);
+            $table->tinyInteger('number');
+            $table->bigInteger('big_number');
+            $table->text('string');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('extras');
     }
 };
